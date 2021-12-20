e4be93fe31f77df478179815fa7bd60480b175f6
Wakil Khan <wakil.khan96@gmail.com>
2021-12-20 16:55:00 +0600

    Flutter

--- Flutter/doctime_ui_build/lib/screens/ui_screens/screen3.dart
+++ Flutter/doctime_ui_build/lib/screens/ui_screens/screen3.dart
@@ -8,6 +8,12 @@ class CardItem {
   // final String subtitle;
   const CardItem({required this.icon, required this.title});
 }
+class ImageCardItem {
+  final String urlImage;
+  final String title;
+  final String subtitle;
+  const ImageCardItem({required this.urlImage, required this.title, required this.subtitle});
+}
 
 class UIScreen3 extends StatefulWidget {
   const UIScreen3({Key? key}) : super(key: key);
@@ -18,15 +24,23 @@ class UIScreen3 extends StatefulWidget {
 
 class _UIScreen3State extends State<UIScreen3> {
   List<CardItem> items = [];
+  List<ImageCardItem> imageItems = [];
 
   _UIScreen3State() {
-    String url = "https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80";
+    // String url = "https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80";
+    String url = "https://images.unsplash.com/photo-1612531386530-97286d97c2d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
 
     for (int i = 0; i < 5; i++) {
       // items.add(CardItem(urlImage: url, title: "Text inside container.", subtitle: "10\$"));
       items.add(CardItem(icon: Icons.account_circle_outlined, title: "General Physician"));
     }
+
+
+    for (int j = 0;  j < 10; j++) {
+      imageItems.add(ImageCardItem(urlImage: url, title: "NIKE Shoe", subtitle: "10\$"));
+    }
     print(items.length);
+    print(imageItems.length);
   }
 
   @override
@@ -146,7 +160,7 @@ class _UIScreen3State extends State<UIScreen3> {
                         // padding: EdgeInsets.all(5),
                         scrollDirection: Axis.horizontal,
                         itemBuilder: (context, index) => buildCard(items[index]),
-                        separatorBuilder: (context, _) => SizedBox(width: 15,),
+                        separatorBuilder: (context, _) => const SizedBox(width: 15,),
                         itemCount: items.length
                       ),
                     ),
@@ -195,6 +209,10 @@ class _UIScreen3State extends State<UIScreen3> {
                     Container(
                       height: 100,
                       decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/video_background.jpg"),
+                          fit: BoxFit.cover,
+                        ),
                         border: Border.all(
                           //color: Colors.grey.withOpacity(0.5),
                           color: Colors.white,
@@ -207,21 +225,26 @@ class _UIScreen3State extends State<UIScreen3> {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Expanded(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Icon(Icons.add),
-                                Text(
-                                  " See how doctime work -> Render Problem.",
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              SizedBox(width: 50,),
+                              Icon(Icons.add, color: Colors.black, ),
+                              Flexible(
+                                child: Text(
+                                  " See how DocTime works",
                                   textAlign: TextAlign.center,
+                                  overflow: TextOverflow.ellipsis,
                                   style: TextStyle(
-                                    fontSize: 16,
                                     fontFamily: 'RobotoMono',
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
                                   ),
                                 ),
-                              ],
-                            ),
+                              ),
+                              SizedBox(width: 50,),
+                            ],
                           ),
                         ],
                       ),
@@ -234,7 +257,7 @@ class _UIScreen3State extends State<UIScreen3> {
 
                     Row(
                       //mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
+                      children: const [
                         Text(
                           "Recently viewed",
                           textAlign: TextAlign.center,
@@ -247,33 +270,124 @@ class _UIScreen3State extends State<UIScreen3> {
                       ],
                     ),
                     const SizedBox(
-                      height: 10,
+                      height: 15,
                     ),
                     Container(
-                      child: const Expanded(
-                        flex: 0,
-                        child: TextField(
-                          // controller: descriptionTextFieldController,
-                          maxLines: null, //wrap text
-                          autofocus: true,
-                          autocorrect: true,
-                          enabled: false, // editing permission
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            height: 1.8, // line spacing height
-                            fontSize: 18,
-                            wordSpacing: 2.00,
-                          ),
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: '',
-                            contentPadding: EdgeInsets.zero,
-                          ),
-                          //showCursor: false,
-                          //cursorHeight: 18,
-                        ),
+                      height: 100,
+                      child: ListView.separated(
+                        // padding: EdgeInsets.all(5),
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) => buildImageCard(imageItems[index]),
+                          separatorBuilder: (context, _) => const SizedBox(width: 10,),
+                          itemCount: imageItems.length
                       ),
                     ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+
+                    Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          //color: Colors.grey.withOpacity(0.5),
+                          color: Colors.white,
+                          width: 0.00,
+                        ),
+                        // boxShadow: const [
+                        //   BoxShadow(
+                        //     color: Color.fromRGBO(0, 0, 0, 0.03),
+                        //     offset: Offset(0, 0),
+                        //     blurRadius: 2,
+                        //     spreadRadius: 2,
+                        //   )
+                        // ],
+                        borderRadius: BorderRadius.circular(30.0),
+                        color: const Color.fromRGBO(238, 244, 255, 1.0),
+                        // color: Colors.transparent,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+
+                              Text(
+                                "Share ",
+                                style: TextStyle(
+                                  fontFamily: 'RobotoMono',
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              Text(
+                                "DocTime",
+                                style: TextStyle(
+                                    fontFamily: 'RobotoMono',
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.lightBlue
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text(
+                                "with family and friends",
+                                style: TextStyle(
+                                    fontFamily: 'RobotoMono',
+                                    fontSize: 23,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          const SizedBox(height: 12),
+
+                          Container(
+                            height: 50,
+                            width: 320,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30.0),
+                              color: Colors.blue,
+                              // color: Colors.transparent,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Expanded(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const [
+                                      Icon(Icons.add, size: 16, color: Colors.white,),
+                                      Text(
+                                        "SHARE WITH YOUR FRIENDS",
+                                        style: TextStyle(
+                                          fontFamily: 'RobotoMono',
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+
+                          ),
+
+                        ],
+                      ),
+                    ),
+
                     const SizedBox(height: 30),
                   ],
                 ),
@@ -287,40 +401,113 @@ class _UIScreen3State extends State<UIScreen3> {
 }
 
 
+
+
 Widget buildCard(CardItem item) {
 
-  return Container(
-    width: 130,
-    decoration: BoxDecoration(
-      // border: Border.all(
-      //   //color: Colors.grey.withOpacity(0.5),
-      //   color: Colors.white,
-      //   width: 0.00,
-      // ),
-      borderRadius: BorderRadius.circular(10.0),
-      color: const Color.fromRGBO(238, 244, 255, 1.0),
-    ),
-    child: Column(
-      children: [
-        const Expanded(child: SizedBox(width: 1)),
+  return Padding(
+    // padding: EdgeInsets.all(10),
+    padding: EdgeInsets.only(top: 5, bottom: 5),
+    child: Container(
+      width: 130,
+      decoration: BoxDecoration(
+        // border: Border.all(
+        //   //color: Colors.grey.withOpacity(0.5),
+        //   color: Colors.white,
+        //   width: 0.00,
+        // ),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.07),
+            offset: Offset(0, 0),
+            blurRadius: 7,
+            spreadRadius: 2,
+          )
+        ],
+        borderRadius: BorderRadius.circular(10.0),
+        color: const Color.fromRGBO(238, 244, 255, 1.0),
+      ),
+      child: Column(
+        children: [
+          const Expanded(child: SizedBox(width: 1)),
 
-        Icon(
-          item.icon,
-          size: 50,
-        ),
-        SizedBox(height: 10,),
-        Text(
-          item.title,
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-              fontFamily: 'RobotoMono',
-              fontSize: 18,
-              // fontWeight: FontWeight.bold,
+          Icon(
+            item.icon,
+            size: 50,
           ),
-        ),
+          SizedBox(height: 10,),
+          Text(
+            item.title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+                fontFamily: 'RobotoMono',
+                fontSize: 18,
+                // fontWeight: FontWeight.bold,
+            ),
+          ),
 
-        const Expanded(child: SizedBox(width: 1)),
-      ],
+          const Expanded(child: SizedBox(width: 1)),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget buildImageCard(ImageCardItem item) {
+
+  return Padding(
+    padding: EdgeInsets.all(0),
+    // padding: EdgeInsets.only(top: 1, bottom: 1),
+    child: Container(
+      width: 100,
+      decoration: BoxDecoration(
+        border: Border.all(
+          //color: Colors.grey.withOpacity(0.5),
+          color: Colors.white,
+          width: 0.00,
+        ),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.07),
+            offset: Offset(0, 0),
+            blurRadius: 7,
+            spreadRadius: 2,
+          )
+        ],
+        borderRadius: BorderRadius.circular(30.0),
+        color: const Color.fromRGBO(238, 244, 255, 1.0),
+        // color: Colors.transparent,
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: AspectRatio(
+              aspectRatio: 4 / 3,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30.0),
+                child: Image.network(
+                  item.urlImage,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          // Text(
+          //   item.title,
+          //   style: TextStyle(
+          //     fontSize: 18,
+          //     fontWeight: FontWeight.bold,
+          //   ),
+          // ),
+          // Text(
+          //   item.subtitle,
+          //   style: TextStyle(
+          //     fontSize: 14,
+          //     fontWeight: FontWeight.bold,
+          //   ),
+          // ),
+        ],
+      ),
     ),
   );
 }
